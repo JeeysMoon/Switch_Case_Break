c1bc6ba59ff506accaffd1d55e5a2fdedcb36dad
Jennifer Sousa <jennifergaldinodesousa@gmail.com>
2022-01-28 20:37:44 -0300

    2801

---
+++ src/exercício_de_revisão2_04_09/Exercício_de_Revisão2_04_09.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercício_de_revisão2_04_09;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jennifer
+ */
+public class Exercício_de_Revisão2_04_09 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Scanner leitor = new Scanner(System.in);
+         String periodo;
+         
+    switch (periodo) {
+        case "M":
+            System.out.println("Bom dia");
+            break;
+        case "V":
+            System.out.println("Boa tarde");
+            break;
+            case "N":
+            System.out.println("Boa noite");
+            break;
+            
+        }
+    }
+    
+}
